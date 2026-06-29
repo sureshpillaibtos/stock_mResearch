911b255c06e421f4f49e61048b5c751ab1e916a4
sureshpillaibtos <psk_suresh@yahoo.com>
2026-06-29 14:20:07 +0530

    Delete stock_mResearch directory

--- stock_mResearch/indicators/moving_average.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def add_sma(df, length):
-    df[f'SMA{length}'] = df['Close'].rolling(window=length).mean()
-    return df
-
-def add_ema(df, length):
-    df[f'EMA{length}'] = df['Close'].ewm(span=length, adjust=False).mean()
-    return df
